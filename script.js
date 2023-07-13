70905e11967001108fca5e195d080bb0955a723e
ixyhoo <ixyhoo@outlook.fr>
2023-07-13 06:16:12 +0200

    geral setting

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const img = new Image();
+img.src = "./media/flappy-bird-set.png";
+
+// General settings
+
+let gamePlaying = false;
+const gravity = 0.5;
+const speed = 6.2;
+const size = [51, 36];
+const jump = -11.5;
+const cTenth = (canvas.width / 10);
+
+let index = 0,
+    bestScore = 0,
+    currentScore = 0,
+    pipes = [],
+    flight,
+    flyHeight;
+
 
